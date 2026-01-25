147c20041b54df4b41ef217416e4f2f02d3dc183
dalolos-dev <dalolos@outlook.es>
2026-01-25 17:19:44 +0100

    feat: add JPA repositories for entities in repository layer

---
+++ src/main/java/com/dalolos/employee/management/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.dalolos.employee.management.repository;
+
+import com.dalolos.employee.management.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
