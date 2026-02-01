24e1e38b4e0d7be0791ef80a3266e70672604c6e
dalolos-dev <dalolos@outlook.es>
2026-02-01 19:42:49 +0100

    chore: remove unused libraries

--- src/main/java/com/dalolos/employee/management/config/SecurityConfig.java
+++ src/main/java/com/dalolos/employee/management/config/SecurityConfig.java
@@ -3,8 +3,6 @@ package com.dalolos.employee.management.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
